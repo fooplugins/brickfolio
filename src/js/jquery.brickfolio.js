b7cb805149a5a6abf90aa4b76a5fa7d10314db8f
Brad Vincent <bradvin@gmail.com>
2014-07-16 22:11:49 +0200

    added js header

--- src/js/jquery.brickfolio.js
+++ src/js/jquery.brickfolio.js
@@ -1,3 +1,12 @@
+/*jslint devel: true, browser: true, unparam: true, debug: false, es5: true, white: false, maxerr: 1000 */
+/**!
+ * Brickfolio - A jQuery plugin for equally spaced grid layouts
+ * @version 0.0.1
+ * @link https://github.com/fooplugins/brickfolio
+ * @copyright Steven Usher & Brad Vincent 2014
+ * @license Released under the GPL license.
+ * You are free to use Brickfolio in personal projects as long as this copyright header is left intact.
+ */
 (function($){
 
 	/**
